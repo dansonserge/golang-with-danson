3f6448d792c4ffed7d275df1b15133c188340c3b
Serge Danson <sergedanson@gmail.com>
2025-03-20 23:42:20 +0200

    middleware logging

--- gin-gorm/routes/user.go
+++ gin-gorm/routes/user.go
@@ -2,11 +2,13 @@ package routes
 
 import (
 	"gin-gorm/controller"
+	"gin-gorm/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoute(route *gin.Engine) {
+	route.Use(middleware.LoggerMiddleware())
 	route.GET("/", controller.ReGetUser)
 	route.GET("/:id", controller.ReGetUserById)
 	route.POST("/", controller.CreateUser)
